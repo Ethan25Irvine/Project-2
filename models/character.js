5921212c9960717d79df101a2b99e372ea207147
Ethan Irvine <edog.irvine@gmail.com>
2020-03-07 12:50:39 -0700

    Added more to the database (all the proficiences and death saves)

--- models/character.js
+++ models/character.js
@@ -1,6 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
     const Character = sequelize.define("Character", {
+    //    Basic Character Traits
+    // ######################################
         Name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        Class: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
@@ -28,6 +37,22 @@ module.exports = function(sequelize, DataTypes) {
             len: [1]
             }
         },
+        XP: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        Level: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        // Character Stats
+        // ####################################
         Strength: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -70,6 +95,8 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
+        // Additional Character Stats
+        // ##############################################
         ArmorClass: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -97,7 +124,149 @@ module.exports = function(sequelize, DataTypes) {
             validate: {
                 len: [1]
             }
-        }
+        },
+        // Death Saves
+        // ##########################
+        Successes:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+            validate: {
+                len: [1]
+            }
+        },
+        Failures:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+            validate: {
+                len: [1]
+            }
+        },
+        // Ability Saving Throws
+        // #################################
+        StrengthST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        DexterityST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        ConstitutionST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        IntelligenceST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        WisdomST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        CharismaST: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        // Proficiencies 
+        // ################################
+        Acrobatics: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        AnimalHandling: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Arcana: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Athletics: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Deception: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        History: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Insight: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Intimidation: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Investigation: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Medicine: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Nature: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Perception: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Performance: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Persuasion: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Religion: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        SlightOfHand: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Stealth: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        Survival: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
     });
   
     Character.associate = function(models) {
