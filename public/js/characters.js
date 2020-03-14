20f6ac757dc6b3041ac3c28085c8a026cbd781f5
Jordan Albers <albersjordan13@gmail.com>
2020-03-13 18:51:44 -0700

    removed useless images folder, and made a javascript function to create and append cards to the characters.html page

--- public/js/characters.js
+++ public/js/characters.js
@@ -1,7 +1,21 @@
 // this is for characters page
 $(document).ready(function() {
 	$('#create').on('click', function() {
-		const url = './index.html';
-		window.location = url;
+		// const url = './index.html';
+		// window.location = url;
+		const card = $("<div class= 'card cardchars'>");
+		const cardBody = $('<div>');
+		const content = $("<h2 class='card-title text-center medievalsharp'>Melvin</h2>");
+		const attributes = $(
+			"<button type='button' class='btn btn-dark btn-lg btn-block modalbtn' data-toggle='modal' data-target='#exampleModalLong'><h4 class='medievalsharp'>Attributes</h4></button>"
+		);
+		const image = $('<img src="../public/images/647002-Fat_Wizard.png" class="card-img-top" alt="">');
+		$(cardBody).attr('class', 'card-body');
+		$(cardBody).append(content);
+		$(cardBody).append(attributes);
+		$(card).append(image);
+		$(card).append(cardBody);
+
+		$('#characters').append(card);
 	});
 });
